aeee4455559ae4c7b80e59b4665907bfe8941c75
Sarah Martin <blue.howrse@gmail.com>
2022-12-16 21:25:07 -0700

    Added white space on sides of demo after user interview

--- pages/js/demo.js
+++ pages/js/demo.js
@@ -20,7 +20,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth * 0.85, window.innerHeight * 0.75);
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // const verticesOfCube = [
